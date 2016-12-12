dc4bb0b425df2a830429015130c05e2e77fe1de7
Gödecke, Maik <mgoedecke@note-apple-23.local>
2016-12-12 13:29:45 +0100

    Initial Commit

---
+++ appleTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appleTest
+//
+//  Created by Gödecke, Maik on 12/12/16.
+//  Copyright © 2016 moimoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
